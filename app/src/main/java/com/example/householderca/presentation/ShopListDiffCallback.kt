e8cee4cc87dd3bbbb4db2eb82c4bd3658b9cbded
Ajems <danilkorenev2004@gmail.com>
2023-08-10 19:36:29 +0300

    Added DiffUtil for main RecyclerView

---
+++ app/src/main/java/com/example/householderca/presentation/ShopListDiffCallback.kt
@@ -0,0 +1,23 @@
+package com.example.householderca.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.householderca.domain.pojo.ShopItem
+
+class ShopListDiffCallback(
+    private val oldList: List<ShopItem>,
+    private val newList: List<ShopItem>
+): DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].id == newList[newItemPosition].id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        return oldItem == newItem
+    }
+}
