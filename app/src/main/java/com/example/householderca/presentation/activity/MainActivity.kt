0b72cae3fca4dacd7c6a2e3b8a6d5eb11fdf65ed
Ajems <danilkorenev2004@gmail.com>
2023-08-14 19:23:08 +0300

    No StatusBar and NavigationBar design: Done

--- app/src/main/java/com/example/householderca/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/householderca/presentation/activity/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.householderca.presentation.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
@@ -15,6 +20,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var shopListAdapter: ShopListAdapter
+    private lateinit var buttonAddItem: FloatingActionButton
+    private lateinit var recyclerViewShopList: RecyclerView
+    private var statusBarHeight: Int? = null
+    private var navigationBarHeight: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +33,67 @@ class MainActivity : AppCompatActivity() {
         viewModel.shopList.observe(this) {
             shopListAdapter.submitList(it)
         }
+        addButtonAddClickListener()
+        setEdgeToEdgeWindow()
+    }
 
-        val buttonAddItem = findViewById<FloatingActionButton>(R.id.addShopItem)
+    private fun setEdgeToEdgeWindow() {
+        calculateStatusAndNavigationBar()
+        increaseLayoutMargin()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        if (isLightTheme(this)) {
+            val systemDecorColor = (View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR)
+            window.decorView.systemUiVisibility = systemDecorColor
+
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        }
+    }
+
+    private fun isLightTheme(context: Context): Boolean {
+        val currentNightMode = context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return currentNightMode == Configuration.UI_MODE_NIGHT_NO
+    }
+
+
+    private fun increaseLayoutMargin() {
+        val layoutParamsButtonAdd = buttonAddItem.layoutParams as ViewGroup.MarginLayoutParams
+        navigationBarHeight?.let { height ->
+            layoutParamsButtonAdd.bottomMargin += height
+        }
+        buttonAddItem.layoutParams = layoutParamsButtonAdd
+
+        if(statusBarHeight != null && navigationBarHeight != null) {
+            recyclerViewShopList.setPadding(
+                0,
+                statusBarHeight!!,
+                0,
+                navigationBarHeight!!+recyclerViewShopList.paddingBottom
+            )
+        }
+    }
+
+    private fun dpToPx(dp: Double, context: Context): Int {
+        val scale: Float = context.resources.displayMetrics.density
+        return (dp * scale + 0.5f).toInt()
+    }
+
+
+    @SuppressLint("DiscouragedApi", "InternalInsetResource")
+    private fun calculateStatusAndNavigationBar() {
+        val resourceIdSB = resources.getIdentifier("status_bar_height", "dimen", "android")
+        val resourceIdNB = resources.getIdentifier("navigation_bar_height", "dimen", "android")
+        if (resourceIdSB > 0 && resourceIdNB > 0) {
+            statusBarHeight = resources.getDimensionPixelSize(resourceIdSB)
+            navigationBarHeight = resources.getDimensionPixelSize(resourceIdNB)
+        }
+    }
+
+    private fun addButtonAddClickListener() {
+        buttonAddItem = findViewById(R.id.addShopItem)
         buttonAddItem.setOnClickListener {
             val intent = ShopItemActivity.newIntentAddItem(this)
             startActivity(intent)
@@ -33,7 +101,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpRecyclerView(){
-        val recyclerViewShopList = findViewById<RecyclerView>(R.id.recyclerViewShopList)
+        recyclerViewShopList = findViewById<RecyclerView>(R.id.recyclerViewShopList)
 
         with(recyclerViewShopList) {
             shopListAdapter = ShopListAdapter()
